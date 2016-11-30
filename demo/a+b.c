e626d3c18429151d3a6d68b3abfa1c907382e39e
inuyasha2012 <chinachrist@163.com>
2016-11-30 15:54:41 +0800

    init

--- demo/a+b.c
+++ demo/a+b.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main() {
     int a, b;
-    
+    sleep(2);
     scanf("%d %d", &a, &b);
     printf("%d\n", a + b);
     // open("asdf", O_CREAT);
