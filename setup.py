b1b20050a5e1f5309556d47ed64fefa36963c7c2
inuyasha2012 <chinachrist@163.com>
2016-12-02 13:19:11 +0800

    init

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from distutils.core import setup, Extension
 
 sources = [
     'lorun/cext/lorun.c', 'lorun/cext/convert.c', 'lorun/cext/access.c',
-    'lorun/cext/limit.c', 'lorun/cext/run.c', 'lorun/cext/diff.c'
+    'lorun/cext/limit.c', 'lorun/cext/run.c', 'lorun/cext/diff.c',
+    'lorun/cext/proc.c'
 ]
 
 setup(name='lorun',
